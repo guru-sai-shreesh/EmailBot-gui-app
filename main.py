8289f667ca250db8feed593052a80427fa356b4c
Guru sai shreesh Tirumalla <guru.sai.shreesh@gmail.com>
2021-10-03 16:02:16 +0530

    changes according for new proj

--- main.py
+++ main.py
@@ -9,12 +9,95 @@ from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.list import TwoLineAvatarListItem, IconLeftWidget
 import copy
+# for listener to work install pyaudio package
 
 email_receivers = []
 addresses = []
 sub = []
 body = []
 new_contacts = []
+
+wb = xl.load_workbook('contacts.xlsx')
+sheet = wb['Sheet1']
+contact_list = {}
+x = 2
+for x in range(2, sheet.max_row + 1):
+    cell1 = sheet.cell(x, 2)
+    cell2 = sheet.cell(x, 3)
+    contact_list[cell1.value] = cell2.value
+
+listener = sr.Recognizer()
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 180)  # reduces WPM to 180
+engine.setProperty('voice', 'com.apple.speech.synthesis.voice.samantha.premium')  # This voice is best suited in macos
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def mike_out():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            info = listener.recognize_google(voice)
+            print(info)
+            return info.lower()
+    except:
+        pass
+
+
+def send_email(receiver, subject, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587)  # to connect to server
+    server.starttls()  # tells that it is an secure connection
+    # Make sure to give app access in your Google account
+    server.login('gojo.testing123@gmail.com', 'hellogojo')
+    email = EmailMessage()
+    email['From'] = 'gojo.testing123@gmail.com'
+    email['To'] = receiver
+    email['Subject'] = subject
+    email.set_content(message)
+    server.send_message(email)
+
+
+def gather_and_send():
+    subject0 = ""
+    body0 = ""
+    i = 0
+    j = 0
+    for subs in sub:
+        if i == 0:
+            subject0 = subs.capitalize()
+            i += 1
+        else:
+            subject0 += ('. ' + subs.capitalize())
+    for bodies in body:
+        if j == 0:
+            body0 = bodies.capitalize()
+            j += 1
+        else:
+            body0 += ('. ' + bodies.capitalize())
+    unique_addresses = list(set(addresses))
+    for receiver in unique_addresses:
+        send_email(receiver, subject0, body0)
+    print('Email sent successfully to', *unique_addresses)
+
+
+def new_contact():
+    for name, email in new_contacts:
+        contact_list[name] = email
+        new_cell0 = sheet.cell(x + 1, 1)
+        new_cell1 = sheet.cell(x + 1, 2)
+        new_cell2 = sheet.cell(x + 1, 3)
+        new_cell0.value = x - 1
+        new_cell1.value = name
+        new_cell2.value = email
+        wb.save('contacts.xlsx')
+    wb.close()
+
 
 screen_helper = """
 ScreenManager:
@@ -24,9 +107,8 @@ ScreenManager:
     SubjectScreen:
     BodyScreen:
     EndScreen:
-
-
 <MenuScreen>:
+    name: 'menu'
     Image:
         source: 'bot_anim.gif'
         anim_delay: 0.03
@@ -47,19 +129,12 @@ ScreenManager:
         pos_hint: {'center_x':0.8,'center_y':0.14}
         on_press: root.manager.current = 'select'
         elevation: 10
-
 <SelectScreen>:
     name: 'select'
     BoxLayout:
         ScrollView:
             MDList:
                 id: scroll
-    MDLabel:
-        text: 'First select default contacts'
-        bold: True
-        font_size: '17sp'
-        pos_hint: {'center_x': 0.88, 'center_y': 0.954}
-        color: 0.4, 0.4, 0.4, 1
     MDRaisedButton:
         text: 'New contact'
         pos_hint: {'center_x':0.2,'center_y':0.05}
@@ -74,7 +149,7 @@ ScreenManager:
         text: 'Next'
         pos_hint: {'center_x':0.88,'center_y':0.05}
         on_press: root.manager.current = 'subject'
-        
+
 <NCScreen>:
     name: 'new_contact'
     MDLabel:
@@ -126,7 +201,6 @@ ScreenManager:
         text: 'Next'
         pos_hint: {'center_x': 0.88, 'center_y': 0.05}
         on_release: root.manager.current = 'subject'
-
 <SubjectScreen>:
     name: 'subject'
     MDLabel:
@@ -140,18 +214,15 @@ ScreenManager:
         text: "Press on 'start listening' to speak subject"
         pos_hint: {'center_x':0.5,'center_y':0.5}
         size_hint: 0.9, 0.8
-
     MDRaisedButton:
         text: 'Start listening'
         pos_hint: {'center_x':0.8,'center_y':0.95}
         md_bg_color: app.theme_cls.primary_dark
         on_press: root.listen_subject()
-
     MDRaisedButton:
         text: 'Next'
         pos_hint: {'center_x':0.87,'center_y':0.05}
         on_press: root.manager.current = 'body'
-
 <BodyScreen>:
     name: 'body'
     MDLabel:
@@ -165,18 +236,15 @@ ScreenManager:
         text: "Press on 'start listening' to speak BODY"
         pos_hint: {'center_x':0.5,'center_y':0.5}
         size_hint: 0.9, 0.8
-
     MDRaisedButton:
         text: 'Start listening'
         pos_hint: {'center_x':0.8,'center_y':0.95}
         md_bg_color: app.theme_cls.primary_dark
         on_press: root.listen_body()
-
     MDRaisedButton:
         text: 'Next'
         pos_hint: {'center_x':0.87,'center_y':0.05}
         on_press: root.manager.current = 'end'
-
 <EndScreen>:
     name: 'end'
     MDLabel:
@@ -195,11 +263,7 @@ ScreenManager:
         id: notify
         pos_hint: {'center_x':0.57,'center_y':0.05}
         color: 0.16, 0.47, 0.45, 1
-    MDRaisedButton:
-        id: back
-        pos_hint: {'center_x':0.87,'center_y':0.05}
-        on_press: root.manager.current = 'menu'
-        
+
 """
 
 
@@ -209,16 +273,18 @@ class MenuScreen(Screen):
 
 class SelectScreen(Screen):
 
-    @staticmethod
-    def receiver_addresses():
-        receivers_str = mike_out()
-        receivers = receivers_str.split(' and ')
-        for receiver in receivers:
-            if receiver not in contact_list:
-                receivers.remove(receiver)
-        if len(receivers) == 0:
-            talk("sorry, there are no similar email addresses in your contacts")
-            talk("Please try again")
+    def receiver_addresses(self):
+        while(1):
+            receivers_str = mike_out()
+            receivers = receivers_str.split(' and ')
+            for receiver in receivers:
+                if receiver not in contact_list:
+                    receivers.remove(receiver)
+            if len(receivers) == 0:
+                talk("sorry, there are no similar email addresses in your contacts")
+                talk("Please try again")
+            else:
+                break
 
         for receiver in receivers:
             email_receivers.append(receiver.capitalize())
@@ -286,8 +352,6 @@ class EndScreen(Screen):
         for receiver in unique_receivers:
             final_msg_names += (' ' + receiver)
         self.ids.final.text = "Email(s) successfully sent to\n" + final_msg_names
-        self.ids.notify.text = "To send New Email Press here->"
-        self.ids.back.text = "Back"
 
 
 # Create the screen manager
@@ -300,87 +364,6 @@ sm.add_widget(BodyScreen(name='body'))
 sm.add_widget(EndScreen(name='end'))
 
 Window.size = (360, 600)
-
-wb = xl.load_workbook('contacts.xlsx')
-sheet = wb['Sheet1']
-contact_list = {}
-x = 2
-for x in range(2, sheet.max_row+1):
-    cell1 = sheet.cell(x, 2)
-    cell2 = sheet.cell(x, 3)
-    contact_list[cell1.value] = cell2.value
-
-listener = sr.Recognizer()
-
-engine = pyttsx3.init()
-engine.setProperty('rate', 180)  # reduces WPM to 180
-engine.setProperty('voice', 'com.apple.speech.synthesis.voice.samantha.premium')  # This voice is best suited in macos
-
-
-def talk(text):
-    engine.say(text)
-    engine.runAndWait()
-
-
-def mike_out():
-    try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            info = listener.recognize_google(voice)
-            print(info)
-            return info.lower()
-    except:
-        pass
-
-
-def send_email(receiver, subject, message):
-    server = smtplib.SMTP('smtp.gmail.com', 587)  # to connect to server
-    server.starttls()  # tells that it is an secure connection
-    # Make sure to give app access in your Google account
-    server.login('gojo.testing123@gmail.com', 'hellogojo')
-    email = EmailMessage()
-    email['From'] = 'gojo.testing123@gmail.com'
-    email['To'] = receiver
-    email['Subject'] = subject
-    email.set_content(message)
-    server.send_message(email)
-
-
-def gather_and_send():
-    Subject = ""
-    Body = ""
-    i = 0
-    j = 0
-    for subs in sub:
-        if i == 0:
-            Subject = subs.capitalize()
-            i += 1
-        else:
-            Subject += ('. ' + subs.capitalize())
-    for bodies in body:
-        if j == 0:
-            Body = bodies.capitalize()
-            j += 1
-        else:
-            Body += ('. ' + bodies.capitalize())
-    unique_addresses = list(set(addresses))
-    for receiver in unique_addresses:
-        send_email(receiver, Subject, Body)
-    print('Email sent successfully to', *unique_addresses)
-
-
-def new_contact():
-    for name, email in new_contacts:
-        contact_list[name] = email
-        new_cell0 = sheet.cell(x + 1, 1)
-        new_cell1 = sheet.cell(x + 1, 2)
-        new_cell2 = sheet.cell(x + 1, 3)
-        new_cell0.value = x - 1
-        new_cell1.value = name
-        new_cell2.value = email
-        wb.save('contacts.xlsx')
-    wb.close()
 
 
 class DemoApp(MDApp):
